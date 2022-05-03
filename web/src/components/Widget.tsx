5feddccbc19b6885752efd2b65971a67514e3c39
Gean634N <gean.a.l.c@gmail.com>
2022-05-03 10:34:58 -0400

    cria e estiliza o componente  popup Widget

---
+++ web/src/components/Widget.tsx
@@ -0,0 +1,28 @@
+import { ChatTeardropDots } from "phosphor-react";
+import { useState } from "react";
+import { Popover } from '@headlessui/react';
+
+export function Widget() {
+  // fazendo o popup aparecer através de uma variável de estado;
+  // const [isWidgetOpen, setIsWidgetOpen] = useState(false);
+
+  // function toggleWidgetVisibility() {
+  //   setIsWidgetOpen(!isWidgetOpen);
+  // }
+
+  return (
+    <Popover className=" absolute bottom-5 right-5">
+      {/* verifica o estado de isWidgetOpen e caso verdadeiro mostra o paragrafo 'Hello World!!!' */}
+      {/* {isWidgetOpen && <p>Hello World!!!</p> } */}
+      <Popover.Panel>Hello World!!!</Popover.Panel>
+      <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
+        <ChatTeardropDots className="w-6 h-6"/>
+        
+        <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
+          <span className="pl-2"></span>
+          Feedback
+        </span>
+      </Popover.Button>
+    </Popover>
+  );
+};
