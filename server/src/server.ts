a98ebbf891c6bf2c84b644e2098b662b96ea7cdf
Gean634N <gean.a.l.c@gmail.com>
2022-05-04 11:20:45 -0400

    configura ORM prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,31 @@
 import express from 'express'
+import { prisma } from './prisma';
 
 const PORT = 3333;
 
 const app = express();
 
-app.get('/', (_req, res) => {
-  return res.json({ message: 'Hello  World!!!' })
-})
+app.use(express.json());
+
+app.post('/feedbacks', async (req, res) => {
+  const { type, comment, screenshot } = req.body;
+
+  console.log(type, comment, screenshot);
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    }
+  });
+  
+  return res.status(201).json({data: feedback});
+});
 
 app.listen(PORT, () => {
   console.log(`HTTP server running on port ${PORT}`);
-})
+});
+
+// SQlite
+// Prisma
