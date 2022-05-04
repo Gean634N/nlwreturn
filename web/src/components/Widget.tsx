50755bcd5d2f53b2472d930d5c4228c8a1caff37
Gean634N <gean.a.l.c@gmail.com>
2022-05-03 20:49:31 -0400

    cria o frontend de do widget

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,21 +1,17 @@
 import { ChatTeardropDots } from "phosphor-react";
-import { useState } from "react";
 import { Popover } from '@headlessui/react';
+import { WidgetForm } from './WidgetForm';
 
 export function Widget() {
-  // fazendo o popup aparecer através de uma variável de estado;
-  // const [isWidgetOpen, setIsWidgetOpen] = useState(false);
-
-  // function toggleWidgetVisibility() {
-  //   setIsWidgetOpen(!isWidgetOpen);
-  // }
 
   return (
-    <Popover className=" absolute bottom-5 right-5">
-      {/* verifica o estado de isWidgetOpen e caso verdadeiro mostra o paragrafo 'Hello World!!!' */}
-      {/* {isWidgetOpen && <p>Hello World!!!</p> } */}
-      <Popover.Panel>Hello World!!!</Popover.Panel>
-      <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
+    <Popover className="absolute bottom-5 md:bottom-8 right-5 md:right-8 flex flex-col items-end">
+      <Popover.Panel>
+        <WidgetForm />
+      </Popover.Panel>
+      <Popover.Button 
+        className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group"
+      >
         <ChatTeardropDots className="w-6 h-6"/>
         
         <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,13 +1,22 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable global-require */
 module.exports = {
   content: ['./src/**/*.tsx'],
   theme: {
     extend: {
       colors: {
         brand: {
+          300: '#9960FF',
           500: '#8257e6',
         },
       },
+      borderRadius: {
+        md: '4px',
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('tailwind-scrollbar'),
+  ],
 };
