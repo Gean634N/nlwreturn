e06ec23b809e74a8fecc51d48ee4bb1e98e94b83
Gean634N <gean.a.l.c@gmail.com>
2022-05-08 15:08:01 -0400

    :up:

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -5,8 +5,6 @@ import { SubmitFeedbackUseCase } from "./useCases/submitFeedbackUseCase";
 
 export const routes = Router();
 
-routes.get('/', (req, res) => res.json({ message: "test de coneção" }))
-
 routes.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot } = req.body;
 
